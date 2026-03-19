0ba50f7d5d219e20ae435a130fb898ca3f2ae640
yuliiatkachenko <ecspetra@gmail.com>
2026-03-19 13:38:29 -0700

    2026 changes

--- src/handlers/fetchDefaultCitiesList.tsx
+++ src/handlers/fetchDefaultCitiesList.tsx
@@ -3,54 +3,55 @@ import {addCity} from "../redux/CitiesReducer";
 import {Dispatch} from "react";
 import {Action} from "redux";
 import {ICity} from "../types";
+import { API_KEY } from "../apiConstants/apiConstants";
 
 export const fetchDefaultCitiesList = async (
 	src: string,
 	dispatch: Dispatch<Action>
 ) => {
-	console.log("src:",src);
-	return new Promise<object | null>(async (resolve) => {
-		try {
-			const fetchedData = await fetchDataFromAPI(src);
+	try {
+		const CITY_ID = 2643743; // Лондон
 
-			if (!fetchedData?.list) {
-				resolve(null);
-				return;
-			}
+		const res = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&units=metric&appid=${API_KEY}`
+		);
 
-			fetchedData.list.forEach((item: ICity) => {
-				const newCity = {
-					id: item.id,
-					name: item.name,
-					main: {
-						humidity: item.main.humidity,
-						pressure: item.main.pressure,
-						temp: item.main.temp,
-					},
-					sys: {
-						country: item.sys.country,
-						sunrise: item.sys.sunrise,
-						sunset: item.sys.sunset,
-					},
-					weather: [
-						{
-							id: item.weather[0].id,
-							description: item.weather[0].description,
-							icon: item.weather[0].icon,
-						},
-					],
-					wind: {
-						speed: item.wind.speed,
-					},
-				};
+		const item = await res.json();
 
-				dispatch(addCity(newCity));
-			});
-
-			resolve(fetchedData);
-		} catch (e) {
-			console.error(e);
-			resolve(null);
+		if (!res.ok || !item) {
+			return null;
 		}
-	});
+
+		const newCity = {
+			id: item.id,
+			name: item.name,
+			main: {
+				humidity: item.main.humidity,
+				pressure: item.main.pressure,
+				temp: item.main.temp,
+			},
+			sys: {
+				country: item.sys.country,
+				sunrise: item.sys.sunrise,
+				sunset: item.sys.sunset,
+			},
+			weather: [
+				{
+					id: item.weather[0].id,
+					description: item.weather[0].description,
+					icon: item.weather[0].icon,
+				},
+			],
+			wind: {
+				speed: item.wind.speed,
+			},
+		};
+
+		dispatch(addCity(newCity));
+
+		return item;
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
 };
